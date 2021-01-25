c163f0bad991debde5b0488c0b77c3e56c1793af
Alvin Gomez <alvingomez.world@gmail.com>
2021-01-25 19:44:08 +0800

    Hiding clear users button
    The clear button should not appear if there are no user(s) and would appear if there are users found.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,8 +8,9 @@ export class Search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired
-    }
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
+    };
     onSubmit = (e) => {
         e.preventDefault();
         //Passing state text to the main app component through props
@@ -34,7 +35,9 @@ export class Search extends Component {
                     onChange={this.onChange} />
                 <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
-            <button className='btn btn-light btn-block'onClick={this.props.clearUsers}>Clear</button>
+                {this.props.showClear && (<button className='btn btn-light btn-block'
+                onClick={this.props.clearUsers}>Clear
+            </button>)}            
             </div>
         )
     }
